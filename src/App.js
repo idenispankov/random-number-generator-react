c69c9837d8d1601d233ea6df0bc367f32ddf33f8
Denis Pankov <idenispankov@gmail.com>
2021-04-11 20:02:45 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
+import { useState } from 'react';
+
 function App() {
-  return <div>Hello</div>;
+  const [minVal, setMinVal] = useState(0);
+  const [maxVal, setMaxVal] = useState(100);
+  const [randomNumber, setRandomNumber] = useState(50);
+
+  function handleRandomNumber() {
+    setRandomNumber(Math.floor(Math.random() * (maxVal - minVal + 1) + minVal));
+    if (maxVal < minVal) {
+      setRandomNumber('');
+    }
+  }
+
+  function onMinChange(e) {
+    setMinVal(+e.target.value);
+  }
+
+  function onMaxChange(e) {
+    setMaxVal(+e.target.value);
+  }
+
+  return (
+    <div className='page'>
+      <div className='page__container'>
+        <div className='randomNumber'>
+          <p>
+            Random Number:{' '}
+            <span className='randomNumber-color'>{randomNumber}</span>
+          </p>
+        </div>
+        <div className='numContainer'>
+          <div className='numContainer__minmax'>
+            <p>Min: </p>
+            <input
+              className='numContainer__input'
+              type='number'
+              value={minVal}
+              onChange={onMinChange}
+            />
+          </div>
+          <div className='numContainer__minmax'>
+            <p>Max: </p>
+            <input
+              className='numContainer__input'
+              type='number'
+              value={maxVal}
+              onChange={onMaxChange}
+            />
+          </div>
+        </div>
+        <button className='button' onClick={handleRandomNumber}>
+          Get Random Number
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
